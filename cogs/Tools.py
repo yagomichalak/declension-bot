cc6ba1d17197b91a20b0a63b526eca31f570ddf7
Yago <yagomichalak@gmail.com>
2024-01-12 02:45:32 -0300

    refactor: Fixed flake8 for cogs\Tools.py

--- cogs/Tools.py
+++ cogs/Tools.py
@@ -14,131 +14,132 @@ TEST_GUILDS = [os.getenv("TEST_GUILD_ID")] if IS_LOCAL else None
 
 
 class Tools(commands.Cog):
-	""" A command for tool commands. """
+    """ A command for tool commands. """
 
-	def __init__(self, client) -> None:
-		""" Class initializing method. """
+    def __init__(self, client) -> None:
+        """ Class initializing method. """
 
-		self.client = client
-		self.session = aiohttp.ClientSession(loop=client.loop)
+        self.client = client
+        self.session = aiohttp.ClientSession(loop=client.loop)
 
-	_synonym = SlashCommandGroup('synonym', 'Finds synonyms for a given word in a given language.', guild_ids=TEST_GUILDS)
-	_antonym = SlashCommandGroup('antonym', 'Finds antonyms for a given word in a given language.', guild_ids=TEST_GUILDS)
+    _synonym = SlashCommandGroup('synonym', 'Finds synonyms for a given word in a given language.', guild_ids=TEST_GUILDS)
+    _antonym = SlashCommandGroup('antonym', 'Finds antonyms for a given word in a given language.', guild_ids=TEST_GUILDS)
 
-	@commands.Cog.listener()
-	async def on_ready(self) -> None:
-		""" Tells when the cog is ready to use. """
+    @commands.Cog.listener()
+    async def on_ready(self) -> None:
+        """ Tells when the cog is ready to use. """
 
-		print('Tools cog is online!')
+        print('Tools cog is online!')
 
-	@slash_command(name="translate", guild_ids=TEST_GUILDS)
-	@commands.cooldown(1, 5, commands.BucketType.user)
-	async def _translate(self, interaction: ApplicationContext, 
-		language: Option(str, name="to_language", description="The language to translate the message to..", required=True), 
-		message: Option(str, name="message", description="The message to translate.", required=True),
-		source_language: Option(str, name="from_language", description="The source language to translate the message from.", required=False)
-	) -> None:
-		""" Translates a message into another language. """
+    @slash_command(name="translate", guild_ids=TEST_GUILDS)
+    @commands.cooldown(1, 5, commands.BucketType.user)
+    async def _translate(self, interaction: ApplicationContext,
+        language: Option(str, name="to_language", description="The language to translate the message to..", required=True),
+        message: Option(str, name="message", description="The message to translate.", required=True),
+        source_language: Option(str, name="from_language", description="The source language to translate the message from.", required=False)
+    ) -> None:
+        """ Translates a message into another language. """
 
-		await interaction.defer(ephemeral=True)
-		current_time = await utils.get_time_now()
+        await interaction.defer(ephemeral=True)
+        current_time = await utils.get_time_now()
 
-		trans = Translator(service_urls=['translate.googleapis.com'])
-		try:
-			if source_language:
-				translation = trans.translate(f'{message}', dest=f'{language}')
-			else:
-				translation = trans.translate(f'{message}',  src=source_language, dest=f'{language}')
-		except ValueError:
-			return await interaction.respond("**Invalid parameter for 'language'!**", ephemeral=True)
+        trans = Translator(service_urls=['translate.googleapis.com'])
+        try:
+            if source_language:
+                translation = trans.translate(f'{message}', dest=f'{language}')
+            else:
+                translation = trans.translate(f'{message}', src=source_language, dest=f'{language}')
+        except ValueError:
+            return await interaction.respond("**Invalid parameter for 'language'!**", ephemeral=True)
 
-		embed = discord.Embed(title="__Translator__",
-			description=f"**Translated from `{translation.src}` to `{translation.dest}`**\n\n{translation.text}",
-			color=interaction.author.color, timestamp=current_time)
-		embed.set_author(name=interaction.author, icon_url=interaction.author.display_avatar)
-		await interaction.respond(embed=embed, ephemeral=True)
+        embed = discord.Embed(title="__Translator__",
+            description=f"**Translated from `{translation.src}` to `{translation.dest}`**\n\n{translation.text}",
+            color=interaction.author.color, timestamp=current_time)
+        embed.set_author(name=interaction.author, icon_url=interaction.author.display_avatar)
+        await interaction.respond(embed=embed, ephemeral=True)
 
-	@_synonym.command(name="french")
-	@commands.cooldown(1, 15, commands.BucketType.user)
-	async def _synonym_french(self, interaction, search: Option(str, name="search", description="The word you are looking for.", required=True)) -> None:
-		""" Searches synonyms of a French word. """
+    @_synonym.command(name="french")
+    @commands.cooldown(1, 15, commands.BucketType.user)
+    async def _synonym_french(self, interaction, search: Option(str, name="search", description="The word you are looking for.", required=True)) -> None:
+        """ Searches synonyms of a French word. """
 
-		await interaction.defer(ephemeral=True)
-		member = interaction.author
-		current_time = await utils.get_time_now()
+        await interaction.defer(ephemeral=True)
+        member = interaction.author
+        current_time = await utils.get_time_now()
 
-		url = f"https://dicolink.p.rapidapi.com/mot/{search.strip().replace(' ', '%20')}/synonymes"
-		querystring = {"limite":"10"}
+        url = f"https://dicolink.p.rapidapi.com/mot/{search.strip().replace(' ', '%20')}/synonymes"
+        querystring = {"limite": "10"}
 
-		headers = {
-			'x-rapidapi-key': os.getenv('RAPID_API_TOKEN'),
-			'x-rapidapi-host': "dicolink.p.rapidapi.com"
-			}
+        headers = {
+            'x-rapidapi-key': os.getenv('RAPID_API_TOKEN'),
+            'x-rapidapi-host': "dicolink.p.rapidapi.com"
+        }
 
-		async with self.session.get(url=url, headers=headers, params=querystring) as response:
-			if response.status != 200:
-				return await interaction.respond(f"**Nothing found, {member.mention}!**", ephemeral=True)
+        async with self.session.get(url=url, headers=headers, params=querystring) as response:
+            if response.status != 200:
+                return await interaction.respond(f"**Nothing found, {member.mention}!**", ephemeral=True)
 
-			data = json.loads(await response.read())
+            data = json.loads(await response.read())
 
-			# Makes the embed's header
-			embed = discord.Embed(
-				title="__French Synonyms__",
-				description=f"Showing results for: {search}",
-				color=member.color,
-				timestamp=current_time
-			)
+            # Makes the embed's header
+            embed = discord.Embed(
+                title="__French Synonyms__",
+                description=f"Showing results for: {search}",
+                color=member.color,
+                timestamp=current_time
+            )
 
-			words = ', '.join(list(map(lambda w: f"**{w['mot']}**", data)))
+            words = ', '.join(list(map(lambda w: f"**{w['mot']}**", data)))
 
-			# Adds a field for each example
-			embed.add_field(name=f"__Words__", value=words, inline=False)
+            # Adds a field for each example
+            embed.add_field(name="__Words__", value=words, inline=False)
 
-			# Sets the author of the search
-			embed.set_author(name=member, icon_url=member.display_avatar)
-			await interaction.respond(embed=embed, ephemeral=True)
+            # Sets the author of the search
+            embed.set_author(name=member, icon_url=member.display_avatar)
+            await interaction.respond(embed=embed, ephemeral=True)
 
-	@_antonym.command(name="french")
-	@commands.cooldown(1, 15, commands.BucketType.user)
-	async def _antonym_french(self, interaction, search: Option(str, name="search", description="The word you are looking for.", required=True)) -> None:
-		""" Searches antonyms of a French word. """
+    @_antonym.command(name="french")
+    @commands.cooldown(1, 15, commands.BucketType.user)
+    async def _antonym_french(self, interaction, search: Option(str, name="search", description="The word you are looking for.", required=True)) -> None:
+        """ Searches antonyms of a French word. """
 
-		await interaction.defer(ephemeral=True)
-		member = interaction.author
-		current_time = await utils.get_time_now()
+        await interaction.defer(ephemeral=True)
+        member = interaction.author
+        current_time = await utils.get_time_now()
 
-		url = f"https://dicolink.p.rapidapi.com/mot/{search.strip().replace(' ', '%20')}/antonymes"
-		querystring = {"limite":"10"}
+        url = f"https://dicolink.p.rapidapi.com/mot/{search.strip().replace(' ', '%20')}/antonymes"
+        querystring = {"limite": "10"}
 
-		headers = {
-			'x-rapidapi-key': os.getenv('RAPID_API_TOKEN'),
-			'x-rapidapi-host': "dicolink.p.rapidapi.com"
-			}
+        headers = {
+            'x-rapidapi-key': os.getenv('RAPID_API_TOKEN'),
+            'x-rapidapi-host': "dicolink.p.rapidapi.com"
+        }
 
-		async with self.session.get(url=url, headers=headers, params=querystring) as response:
-			if response.status != 200:
-				return await interaction.respond(f"**Nothing found, {member.mention}!**", ephemeral=True)
+        async with self.session.get(url=url, headers=headers, params=querystring) as response:
+            if response.status != 200:
+                return await interaction.respond(f"**Nothing found, {member.mention}!**", ephemeral=True)
 
-			data = json.loads(await response.read())
+            data = json.loads(await response.read())
 
-			# Makes the embed's header
-			embed = discord.Embed(
-				title="__French Antonyms__",
-				description=f"Showing results for: {search}",
-				color=member.color,
-				timestamp=current_time
-			)
+            # Makes the embed's header
+            embed = discord.Embed(
+                title="__French Antonyms__",
+                description=f"Showing results for: {search}",
+                color=member.color,
+                timestamp=current_time
+            )
 
-			words = ', '.join(list(map(lambda w: f"**{w['mot']}**", data)))
+            words = ', '.join(list(map(lambda w: f"**{w['mot']}**", data)))
 
-			# Adds a field for each example
-			embed.add_field(name=f"__Words__", value=words, inline=False)
+            # Adds a field for each example
+            embed.add_field(name="__Words__", value=words, inline=False)
 
-			# Sets the author of the search
-			embed.set_author(name=member, icon_url=member.display_avatar)
-			await interaction.respond(embed=embed, ephemeral=True)
+            # Sets the author of the search
+            embed.set_author(name=member, icon_url=member.display_avatar)
+            await interaction.respond(embed=embed, ephemeral=True)
+
 
 def setup(client) -> None:
-	""" Cog's setup function. """
+    """ Cog's setup function. """
 
-	client.add_cog(Tools(client))
+    client.add_cog(Tools(client))
